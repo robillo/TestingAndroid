de77079298bff69a5d5662da64e75ed735a76324
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-09-04 23:51:53 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/androidtesting/OneActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/androidtesting/OneActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 public class OneActivity extends AppCompatActivity {
 
+    //OneActivity
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
